c72759c37edcc8f037b19179cfa2c34d07e6c254
Aditya Verma <adi004.zz@gmail.com>
2018-01-20 03:25:51 +0530

    Fixed few more bugs, added a stop button and more styles

--- script.js
+++ script.js
@@ -11,7 +11,9 @@ let finalAngB = 360 / ($("#displayL").text()*60);
 let i = 0;
 let j = 0;
 let opa;
-//SVG
+
+
+//SVG Logic
 
 
 function polarToCartesian(centerX, centerY, radius, angleInDegrees) {
@@ -42,12 +44,14 @@ function polarToCartesian(centerX, centerY, radius, angleInDegrees) {
     
 
 
-  //Logic
+  //Clock Logic
+
+  //Break Logic
 
     let timeBreak = setInterval(function(){
          
-        if (breakTime){
-            $("#purple").css("opacity", "0");
+        if (breakTime){                                              //Checks if session or break is active   
+            $("#purple").css("opacity", "0");                       //Style change
             $("#red").css("opacity", opa*j);
             $("#timerDiv2").css(
                 {
@@ -61,47 +65,38 @@ function polarToCartesian(centerX, centerY, radius, angleInDegrees) {
             "text-shadow": "0px 0px 10px #FFCF02, 0px 0px 20px #FFCF02, 0px 0px 30px #FFCF02, 0px 0px 40px #FFCF02"
            });
             $("#arc1").attr("stroke", "#FFCF02");
-            $('#timerDiv2 a').css("color", "#00BAD0");
+            $('#timerDiv2 a').css("color", "rgb(255,207,2)");
             document.getElementById("arc1").setAttribute("d", describeArc(125, 125, 113, 0, finalAngB*j));
-           
-            // console.log(setMinsBreak);
             $("#sec").text(secBreak);
             $("#min").text(setMinsBreak);
-            if (!isPause){
-                // console.log(setMinsBreak, secBreak);
+            if (!isPause){                                        //Checks if the clock is paused
                 if (secBreak === 0){
                     if (setMinsBreak ===0){
                         setMinsBreak = $("#displayL").text();
                         secBreak = 1;
                         j = 0;
                         breakTime = !breakTime;
-                        // clearInterval(timeBreak);
-                        console.log(breakTime);
                     }
                     else {
                         secBreak = 60;
                         setMinsBreak--;
-                    }
-                    
+                    }   
                 }
-        
                 secBreak--;
                 j++;
             }
         }
-        
-        
-    
     }, 1000);
     
+
+//Session Logic
+    
     let time = setInterval(function(){
-        // console.log(finalAng);
         if(!breakTime){
-            // console.log(opa*i);
             $("#red").css("opacity", "0");
             $("#purple").css("opacity", opa*i);
             $("#arc1").attr("stroke", "#00BAD0");
-            $('#timerDiv2 a').css("color", "#00BAD0");
+            $('#timerDiv2 a').css("color", "rgb(0,186,208)");
             $("#timerDiv2").css(
                 {
                    "border": "2px #00BAD0 solid",
@@ -115,76 +110,42 @@ function polarToCartesian(centerX, centerY, radius, angleInDegrees) {
             });
             $("#sec").text(sec);
             $("#min").text(setMins);
-            // if ($("#min").text().length === 1){
-            //     $("#min").text("0"+setMins)
-            // }
-            // else {
-            //     $("#min").text(setMins);
-            // }
-            // console.log(typeof $("#min").text())
-            
-        
-        // finalAng = 360/(setMins * 60 + sec);
-        // console.log(finalAng, i);
-        document.getElementById("arc1").setAttribute("d", describeArc(125, 125, 113, 0, finalAng*i));
-            if (!isPause){
-                // console.log(sec);
-                if (sec === 0){
-                    // console.log(sec);
-                    if (setMins ===0){
-                        setMins = $("#displayR").text();
-                        sec = 1;
-                        breakTime = !breakTime;
-                        i = 0;
-                        // clearInterval(time);
-                        // console.log(breakTime);
+            document.getElementById("arc1").setAttribute("d", describeArc(125, 125, 113, 0, finalAng*i));
+                if (!isPause){          
+                    if (sec === 0){
+                        if (setMins ===0){
+                            setMins = $("#displayR").text();
+                            sec = 1;
+                            breakTime = !breakTime;
+                            i = 0;
+                        }
+                        else {
+                            sec = 60;
+                            setMins--;
+                        }
                     }
-                    else {
-                        sec = 60;
-                        setMins--;
-                    }
-                    
-                }
-                // finalAng = finalAng + finalAng;
                 sec--;
                 i++;
             }
         }
-            
-        
-     
-        
     }, 1000);
 
 
 
+//Toggle b/w session and break logic || Play/Pause logic
 
-$("#timerDiv2 a").on("click", function(e){
+$("#timerDiv2 a:nth-of-type(1)").on("click", function(e){
     e.preventDefault();
-    isPause = !isPause;
-    // $("#play").toggleClass("fa-play fa-pause");
-    // $("#play").fadeOut("300", function(){
-    //     $("#play").toggleClass("fa-play fa-pause");
-    // });
-    // $("#play").fadeIn("300", function(){
-    //     $("#play").show();
-    // })
-    if($('#play').hasClass("fa-play"))
-        {
-           // alert();
-         $('#play').fadeOut(500, function() { 
-           
-          
-           $('#play').removeClass('fa-play');
-           $('#play').fadeIn(500, function() { 
-            // $('#play').addClass('fa-pause');
-            $('#play').addClass('fa-pause');
-         });
+    isPause = !isPause;       //Switch!
+    if($('#play').hasClass("fa-play")){
+         $('#play').fadeOut(500, function() {  
+            $('#play').removeClass('fa-play');
         });
-       
-        
-          
-        } else {
+        $('#play').fadeIn(500, function() { 
+            $('#play').addClass('fa-pause');
+        });
+    } 
+    else {
         $('#play').fadeOut(500, function() { 
            $('#play').removeClass('fa-pause');
         });
@@ -196,6 +157,30 @@ $("#timerDiv2 a").on("click", function(e){
     // console.log(isPause, "pause");
     
 });
+
+//Stop/Reset Button
+
+$("#timerDiv2 a:nth-of-type(2)").on("click", function(e){
+    e.preventDefault();
+    setMins = 20;
+    sec = 00;
+    isPause = true;
+    setMinsBreak = 5;
+    secBreak = 0;
+    breakTime = false;
+    finalAng = 360 / ($("#displayR").text()*60);
+    finalAngB = 360 / ($("#displayL").text()*60);
+    i = 0;
+    j = 0;
+    $("#sec").text(sec);
+    $("#min").text(setMins);
+    $("#displayR").text(setMins);
+    $("#displayL").text(setMinsBreak);
+    $('#play').removeClass('fa-pause');
+    $('#play').addClass('fa-play');
+
+});
+//Plus, Minus sign click handlers
 $("#plusL").on("click", function(e){
     e.preventDefault();
     if (!isPause){
@@ -203,21 +188,22 @@ $("#plusL").on("click", function(e){
     }
     else {
         let curVal = Number($("#displayL").text());
-    curVal+= 1;
-    $("#displayL").text(curVal);
-    setMinsBreak = curVal;
-    
-    // sec = 0
-    // setMins+=1;
-    // $("#sec").text(sec);
-    // $("#min").text(setMins);
-    finalAngB = 360 / ($("#displayL").text()*60);
-    opa = 1/($("#displayL").text()*60);
-    j = 0;
-    secBreak = 0;
+        curVal+= 1;
+        if (curVal > 60){
+            curVal = 60;
+            setMinsBreak = curVal;
+        }
+        else {
+            $("#displayL").text(curVal);
+        }
+        finalAngB = 360 / ($("#displayL").text()*60);
+        opa = 1/($("#displayL").text()*60);
+        j = 0;
+        secBreak = 0;
     }
     
 });
+
 $("#minusL").on("click", function(e){
     e.preventDefault();
     if (!isPause){
@@ -232,7 +218,6 @@ $("#minusL").on("click", function(e){
     else {
         $("#displayL").text(curVal);
     }
-    // console.log(curVal);
     setMinsBreak =  $("#displayL").text();
     finalAngB = 360 / ($("#displayL").text()*60);
     opa = 1/($("#displayL").text()*60);
@@ -242,6 +227,7 @@ $("#minusL").on("click", function(e){
     }
     
 });
+
 $("#plusR").on("click", function(e){
     e.preventDefault();
     if (!isPause){
@@ -250,45 +236,48 @@ $("#plusR").on("click", function(e){
     else {
         let curVal = Number($("#displayR").text());
         curVal+= 1;
-        $("#displayR").text(curVal);
         sec = 0;
-        console.log(curVal);
         setMins+=1;
+        if (curVal > 60){
+            curVal = 60;
+            setMins = 60;
+        }
+        else {
+            $("#displayR").text(curVal);
+        }
+
         finalAng = 360/($("#displayR").text()*60);
         opa = 1/($("#displayR").text()*60);
         i = 0;
     }
     
 });
+
 $("#minusR").on("click", function(e){
     e.preventDefault();
-    // $("#displayR").removeClass("animated fadeIn");
     if (!isPause){
 
     }
     else {
-    let curVal = Number($("#min").text());
-    curVal-= 1;
-    // $("#displayR").text(curVal);
-    sec = 0
-    setMins-=1;
+        let curVal = Number($("#min").text());
+        curVal-= 1;
+        sec = 0
+        setMins-=1;
         if (curVal < 1){
-        curval = 1;
-        setMins = 1;
+            curval = 1;
+            setMins = 1;
         }
         else {
             $("#displayR").text(curVal);
         }
-    // console.log(curVal);
-    $("#sec").text(sec);
-    $("#min").text(setMins);
-    finalAng = 360 / ($("#displayR").text()*60);
-    opa = 1/($("#displayR").text()*60);
-    i = 0;
-    console.log(curVal);
-    // console.log(finalArg);
+        $("#sec").text(sec);
+        $("#min").text(setMins);
+        finalAng = 360 / ($("#displayR").text()*60);
+        opa = 1/($("#displayR").text()*60);
+        i = 0;
     }
-    
+
+//Sign hover handler
 });
 $("#minusR, #plusR").hover(
     function(){
@@ -299,71 +288,15 @@ $("#minusR, #plusR").hover(
         $(this).css("color", "white");
     });
 
-    $("#minusL, #plusL").hover(
-        function(){
-            $(this).css("color", "#FFCF02");
-    
-    },
-        function(){
-            $(this).css("color", "white");
-        });
-    
+$("#minusL, #plusL").hover(
+    function(){
+        $(this).css("color", "#FFCF02");
 
-
+},
+    function(){
+        $(this).css("color", "white");
+});
 
 });
-// x60 = 360   i = 6
-// x120 = 360  i = 3
-//
-// Here's my understanding of this. The parameter of a function is local scoped meaning
 
-// ```
-// function bar(a){
-//     console.log(a); //underfined
-// }
-// console.log(a); // Refrence error
-// ```
-
-// a gets hoisted to the top of the function. Js engine intreprts the above code as:
-// ```
-// function bar(a){
-//     var a;
-//     console.log(a); //underfined
-// }
-// console.log(a); // Refrence error
-// ```
-
-// So what happens under the hood of js engine here?:
-// ```
-// function foo(x) {
-// 	x = x + 1;
-//     x; // 3
-// }
-
-// var a = 2;
-// var b = a 
-
-// foo(b);
-// console.log(b); // 2, not 3
-// ```
-
-// Something like this:
-// ```
-// var a;
-// var b;
-// a = 2;
-// b = a; // b = 2
-
-// function foo(x){
-//     var x;
-//     x = b; // These 2 steps happen during hositing of function and the below step happens in the execution phase.
-//     x = x + 1; // since x = b this expression becomes x = b + 1 
-//     console.log(x) // 3
-//     console.log(b) // 2 
-// }
-// foo(b);
-// console.log(b); //2
-// ```
-
-// I honestly had to do a bunch of research to *really* understand this, hope this helps!
-
+//The End.
